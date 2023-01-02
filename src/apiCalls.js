7642c7bef35afc1db2b8e4223dfc48063ad7ff14
Adelle Pitsas <apitsas4@gmail.com>
2023-01-02 17:23:43 -0500

    add post call to api file

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,14 @@ export const fetchTricks = () => {
   return fetch('http://localhost:3001/api/v1/tricks')
   .then(response => response.json())
 }
+
+export const postTrick = (trick) => {
+  return fetch('http://localhost:3001/api/v1/tricks', {
+    method: 'POST',
+    body: JSON.stringify(trick),
+    headers: {
+      'content-type': 'application/json'
+    }
+  })
+  .then(response => response.json())
+}
